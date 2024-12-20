7b00b625910dd7fca0ba6ab1ceee2a8c86fcfc52
S M Ajay Anand <ajayanandsm@gmail.com>
2024-12-21 01:21:27 +0530

    feat: graph updated

--- my-react-app/src/pages/dashboard.jsx
+++ my-react-app/src/pages/dashboard.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "../components/navbar";
 import NavbarVertical from "../components/navbar_vertical";
-import Graph from "../components/graph";
+import ProgressChart from "../components/graph";
+import "../styles/dashboard.css"
 
 const Dashboard = () => {
     return(
         <div className="App">
             <div><Navbar/></div>
             <div><NavbarVertical/></div>
-            <div><Graph/></div>
+            <div className="graph"><ProgressChart/></div>
         </div>
     )
 };
